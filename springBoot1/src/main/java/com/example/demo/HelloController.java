8f801907038e8f4337dd36c4465e502b1c2a9656
fengjutian <2640111081@qq.com>
2021-01-10 11:49:05 +0800

    add

--- springBoot1/src/main/java/com/example/demo/HelloController.java
+++ springBoot1/src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	@RequestMapping("hello")
 	public String Hello() {
 		return "Hello SpringBoot!";
 	}
+	
+    @RequestMapping("/index")
+    public String index(Model model){
+        model.addAttribute("name","hello freemarker");
+        return "index";
+    }
+
 
 }
